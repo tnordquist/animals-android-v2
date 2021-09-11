b2bdb6dd3fd0a198ab57ecc840d240e2f7c1f8f0
Todd Nordquist <toddnordquist144@gmail.com>
2021-09-11 12:36:05 -0400

    Replaced Thread with AsyncTask.

--- app/src/main/java/edu/cnm/deepdive/animalsandroidv2/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/animalsandroidv2/controller/MainActivity.java
@@ -1,10 +1,17 @@
 package edu.cnm.deepdive.animalsandroidv2.controller;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import androidx.appcompat.app.AppCompatActivity;
+import com.squareup.picasso.Picasso;
+import edu.cnm.deepdive.animalsandroidv2.BuildConfig;
 import edu.cnm.deepdive.animalsandroidv2.R;
 import edu.cnm.deepdive.animalsandroidv2.model.Animal;
 import edu.cnm.deepdive.animalsandroidv2.service.WebServiceProxy;
@@ -16,39 +23,69 @@ public class MainActivity extends AppCompatActivity {
 
   private Spinner animalSelector;
   private ArrayAdapter<Animal> adapter;
+  private ImageView image;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     animalSelector = findViewById(R.id.animal_selector);
-    new Retriever().start();
+    image = findViewById(R.id.image);
+    animalSelector.setOnItemSelectedListener(new OnItemSelectedListener() {
+      @Override
+      public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        Animal animal = (Animal) parent.getItemAtPosition(position);
+        if (animal.getImageUrl() != null) {
+          Picasso.get().load(String.format(BuildConfig.CONTENT_FORMAT, animal.getImageUrl()))
+              .into((ImageView) findViewById(R.id.image));
+        }
+      }
+
+      @Override
+      public void onNothingSelected(AdapterView<?> parent) {
+      }
+    });
+    new RetrieverTask().execute();
   }
 
-  private class Retriever extends Thread {
+  private class RetrieverTask extends AsyncTask<Void, Void, List<Animal>> {
 
     @Override
-    public void run() {
+    protected void onPostExecute(List<Animal> animals) {
+      super.onPostExecute(animals);
+      String url = animals.get(0).getImageUrl();
+      adapter = new ArrayAdapter<>(
+          MainActivity.this, R.layout.item_animal_spinner, animals);
+      adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+      if (url != null) {
+        Picasso.get().load(String.format(BuildConfig.CONTENT_FORMAT, url))
+            .into((ImageView) findViewById(R.id.image));
+      }
+      animalSelector.setAdapter(adapter);
+    }
+
+
+    @Override
+    protected List<Animal> doInBackground(Void... voids) {
       try {
         Response<List<Animal>> response = WebServiceProxy.getInstance()
             .getAnimals()
             .execute();
         if (response.isSuccessful()) {
           Log.d(getClass().getName(), response.body().toString());
-          List<Animal> animals = response.body();
-          runOnUiThread(() -> {
-            adapter = new ArrayAdapter<>(
-                MainActivity.this, R.layout.item_animal_spinner, animals);
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
-            animalSelector.setAdapter(adapter);
-          });
+          return response.body();
         } else {
           Log.e(getClass().getName(), response.message());
+          cancel(true);
+          return null;
         }
       } catch (IOException e) {
         Log.e(getClass().getName(), e.getMessage(), e);
+        cancel(true);
+        return null;
       }
     }
   }
 }
+
 
